92c8a04ba9d37c032bdf46201e305d9b67978c34
gazih <gazihand@gmail.com>
2020-02-19 12:27:43 +0300

    19.02

---
+++ app/src/main/java/com/example/girisekran/Users.java
@@ -0,0 +1,176 @@
+package com.example.girisekran;
+
+import java.util.Date;
+
+public class Users {
+    String userIsim;
+    String userSoyisim;
+    int userTelefonNumarası;
+    String userEmail;
+    Date userDogumTarihi;
+    String userMeslek;
+    String userSehir;
+    String userBoy;
+    String userKilo;
+    String userOlcumBel;
+
+    public String getUserIsim() {
+        return userIsim;
+    }
+
+    public void setUserIsim(String userIsim) {
+        this.userIsim = userIsim;
+    }
+
+    public String getUserSoyisim() {
+        return userSoyisim;
+    }
+
+    public void setUserSoyisim(String userSoyisim) {
+        this.userSoyisim = userSoyisim;
+    }
+
+    public int getUserTelefonNumarası() {
+        return userTelefonNumarası;
+    }
+
+    public void setUserTelefonNumarası(int userTelefonNumarası) {
+        this.userTelefonNumarası = userTelefonNumarası;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public Date getUserDogumTarihi() {
+        return userDogumTarihi;
+    }
+
+    public void setUserDogumTarihi(Date userDogumTarihi) {
+        this.userDogumTarihi = userDogumTarihi;
+    }
+
+    public String getUserMeslek() {
+        return userMeslek;
+    }
+
+    public void setUserMeslek(String userMeslek) {
+        this.userMeslek = userMeslek;
+    }
+
+    public String getUserSehir() {
+        return userSehir;
+    }
+
+    public void setUserSehir(String userSehir) {
+        this.userSehir = userSehir;
+    }
+
+    public String getUserBoy() {
+        return userBoy;
+    }
+
+    public void setUserBoy(String userBoy) {
+        this.userBoy = userBoy;
+    }
+
+    public String getUserKilo() {
+        return userKilo;
+    }
+
+    public void setUserKilo(String userKilo) {
+        this.userKilo = userKilo;
+    }
+
+    public String getUserOlcumBel() {
+        return userOlcumBel;
+    }
+
+    public void setUserOlcumBel(String userOlcumBel) {
+        this.userOlcumBel = userOlcumBel;
+    }
+
+    public String getUserOlcumKalca() {
+        return userOlcumKalca;
+    }
+
+    public void setUserOlcumKalca(String userOlcumKalca) {
+        this.userOlcumKalca = userOlcumKalca;
+    }
+
+    public String getUserOlcumGogus() {
+        return userOlcumGogus;
+    }
+
+    public void setUserOlcumGogus(String userOlcumGogus) {
+        this.userOlcumGogus = userOlcumGogus;
+    }
+
+    public String getUserOlcumBacak() {
+        return userOlcumBacak;
+    }
+
+    public void setUserOlcumBacak(String userOlcumBacak) {
+        this.userOlcumBacak = userOlcumBacak;
+    }
+
+    public String getUserHastalıkAdi() {
+        return userHastalıkAdi;
+    }
+
+    public void setUserHastalıkAdi(String userHastalıkAdi) {
+        this.userHastalıkAdi = userHastalıkAdi;
+    }
+
+    public String getUserTahlildeOnemliUnsurlar() {
+        return userTahlildeOnemliUnsurlar;
+    }
+
+    public void setUserTahlildeOnemliUnsurlar(String userTahlildeOnemliUnsurlar) {
+        this.userTahlildeOnemliUnsurlar = userTahlildeOnemliUnsurlar;
+    }
+
+    public String getUserDanisanHikayesi() {
+        return userDanisanHikayesi;
+    }
+
+    public void setUserDanisanHikayesi(String userDanisanHikayesi) {
+        this.userDanisanHikayesi = userDanisanHikayesi;
+    }
+
+    public Users(String userIsim, String userSoyisim, int userTelefonNumarası, String userEmail,
+                 Date userDogumTarihi, String userMeslek, String userSehir, String userBoy,
+                 String userKilo, String userOlcumBel, String userOlcumKalca, String userOlcumGogus,
+                 String userOlcumBacak, String userHastalıkAdi, String userTahlildeOnemliUnsurlar,
+                 String userDanisanHikayesi) {
+        this.userIsim = userIsim;
+        this.userSoyisim = userSoyisim;
+        this.userTelefonNumarası = userTelefonNumarası;
+        this.userEmail = userEmail;
+        this.userDogumTarihi = userDogumTarihi;
+        this.userMeslek = userMeslek;
+        this.userSehir = userSehir;
+        this.userBoy = userBoy;
+        this.userKilo = userKilo;
+        this.userOlcumBel = userOlcumBel;
+        this.userOlcumKalca = userOlcumKalca;
+        this.userOlcumGogus = userOlcumGogus;
+        this.userOlcumBacak = userOlcumBacak;
+        this.userHastalıkAdi = userHastalıkAdi;
+        this.userTahlildeOnemliUnsurlar = userTahlildeOnemliUnsurlar;
+        this.userDanisanHikayesi = userDanisanHikayesi;
+    }
+
+    String userOlcumKalca;
+    String userOlcumGogus;
+    String userOlcumBacak;
+    String userHastalıkAdi;
+    String userTahlildeOnemliUnsurlar;
+    String userDanisanHikayesi;
+
+
+}
