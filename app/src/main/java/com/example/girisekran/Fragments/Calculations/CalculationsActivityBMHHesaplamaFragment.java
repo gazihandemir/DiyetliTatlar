7f0f5b0173552a0ba52a8d9a740e1a03293811b9
gazihandemir <gazihand@gmail.com>
2020-03-16 16:27:39 +0300

    16.03.2020

--- app/src/main/java/com/example/girisekran/Fragments/Calculations/CalculationsActivityBMHHesaplamaFragment.java
+++ app/src/main/java/com/example/girisekran/Fragments/Calculations/CalculationsActivityBMHHesaplamaFragment.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
@@ -11,11 +15,49 @@ import com.example.girisekran.R;
 
 
 public class CalculationsActivityBMHHesaplamaFragment extends Fragment {
+    EditText edCinsiyet, edKilo;
+    TextView tvCevap;
+    Button btnHesapla;
+    static private String cinsiyet = "";
+    static private double kilo = 0;
+    static private double bmhHesabı = 0;
+    static private double oran = 0;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_calculations_activity_b_m_h_hesaplama, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_calculations_activity_b_m_h_hesaplama, container, false);
+        edCinsiyet = rootView.findViewById(R.id.edBmhActCinsiyet);
+        edKilo = rootView.findViewById(R.id.edBmhActKilo);
+        tvCevap = rootView.findViewById(R.id.tvBmhActCevap);
+        tvCevap.setVisibility(View.INVISIBLE);
+        btnHesapla = rootView.findViewById(R.id.btnBmhActHesapla);
+
+        hesaplaBmh();
+        return rootView;
+    }
+    public void hesaplaBmh() {
+btnHesapla.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        cinsiyet = edCinsiyet.getText().toString();
+        kilo = Double.parseDouble(edKilo.getText().toString());
+
+        if (cinsiyet.equalsIgnoreCase("erkek")) {
+            oran = 1;
+        } else if (cinsiyet.equalsIgnoreCase("kadın")) {
+            oran = 0.95;
+        } else {
+            Toast.makeText(getActivity(), "Lütfen Cinsiyetinizi kadın yada erkek olarak giriniz ! ", Toast.LENGTH_SHORT).show();
+        }
+        bmhHesabı = 24 * oran * kilo;
+        String stringMmhHesabı = String.valueOf(bmhHesabı);
+        tvCevap.setVisibility(View.VISIBLE);
+        tvCevap.setText(stringMmhHesabı);
+
+    }
+});
+
     }
 }
