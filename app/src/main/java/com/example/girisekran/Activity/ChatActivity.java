0c0561cd0da1841fd1f9328adbcb92b22ee9ab9c
gazihandemir <gazihand@gmail.com>
2020-03-20 00:21:16 +0300

    20.03.2020

--- app/src/main/java/com/example/girisekran/Activity/ChatActivity.java
+++ app/src/main/java/com/example/girisekran/Activity/ChatActivity.java
@@ -17,7 +17,6 @@ import com.example.girisekran.Adapter.ChatAdapter;
 import com.example.girisekran.Class.ChatModel;
 import com.example.girisekran.R;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -32,6 +31,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 public class ChatActivity extends AppCompatActivity {
     String userName, otherName, userName1;
@@ -44,6 +44,9 @@ public class ChatActivity extends AppCompatActivity {
     RecyclerView chatRecyclerView;
     ChatAdapter chatAdapter;
     List<ChatModel> list;
+    String userKellesi;
+    String kelleName;
+    String gercekKelle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,25 +62,26 @@ public class ChatActivity extends AppCompatActivity {
         chatRecyclerView.setLayoutManager(layoutManager);
         chatAdapter = new ChatAdapter(ChatActivity.this, list, ChatActivity.this, userID.toString());
         chatRecyclerView.setAdapter(chatAdapter);
-        //   bilgileriCek1();
+       // bilgileriCek();
+        kullaniciCek();
     }
 
 
     public void mesajGönder(String text) {
         final Map messageMap = new HashMap();
         messageMap.put("text", text);
-
         String adminMail = "diyetisyenadmin@gmail.com";
         String adminMailFireBase = firebaseUser.getEmail();
         if (adminMail.equals(adminMailFireBase)) {
             messageMap.put("from", userName1);
-            final String key = databaseReference.child("Chat").child(userName1).child(otherName).push().getKey();
-            databaseReference.child("Chat").child(userName1).child(otherName).child(key).setValue(messageMap)
+            final UUID uuid = UUID.randomUUID();
+            final String strRandom = uuid.toString();
+            databaseReference.child("Chat").child(userName1).child(otherName).child(strRandom).setValue(messageMap)
                     .addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
                         public void onComplete(@NonNull Task<Void> task) {
                             if (task.isSuccessful()) {
-                                databaseReference.child("Chat").child(otherName).child(userName1).child(key).setValue(messageMap)
+                                databaseReference.child("Chat").child(otherName).child(userName1).child(strRandom).setValue(messageMap)
                                         .addOnCompleteListener(new OnCompleteListener<Void>() {
                                             @Override
                                             public void onComplete(@NonNull Task<Void> task) {
@@ -90,13 +94,14 @@ public class ChatActivity extends AppCompatActivity {
         } else {
             final String userID = firebaseUser.getUid();
             messageMap.put("from", userID);
-            final String key1 = databaseReference.child("Chat").child(userID).child("Admin").push().getKey();
-            databaseReference.child("Chat").child(userID).child("Admin").child(key1).setValue(messageMap)
+            final UUID uuid = UUID.randomUUID();
+            final String strRandom1 = uuid.toString();
+            databaseReference.child("Chat").child(userID).child("Admin").child(strRandom1).setValue(messageMap)
                     .addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
                         public void onComplete(@NonNull Task<Void> task) {
                             if (task.isSuccessful()) {
-                                databaseReference.child("Chat").child("Admin").child(userID).child(key1).setValue(messageMap)
+                                databaseReference.child("Chat").child("Admin").child(userID).child(strRandom1).setValue(messageMap)
                                         .addOnCompleteListener(new OnCompleteListener<Void>() {
                                             @Override
                                             public void onComplete(@NonNull Task<Void> task) {
@@ -107,70 +112,7 @@ public class ChatActivity extends AppCompatActivity {
                         }
                     });
         }
-
     }
-
-    public void mesajGönder1(String text) {
-        final String userEmail = firebaseUser.getEmail();
-        final String userID = firebaseUser.getUid();
-        final Map messageMap = new HashMap();
-        final String diyetiysyenAdmin = "diyetisyenadmin@gmail.com";
-        messageMap.put("text", text);
-        messageMap.put("from", userEmail.toString());
-        final String key1 = databaseReference.child("Mesajlar").child(userID).child("Admin").push().getKey();
-        final String key2 = databaseReference.child("Mesajlar").child("Admin").child(userID).push().getKey();
-
-        if (diyetiysyenAdmin.equals(userEmail)) {
-            databaseReference.child("Chat").child("Admin").child(userID).child(key2).setValue(messageMap).
-                    addOnCompleteListener(new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            if (task.isSuccessful()) {
-                                databaseReference.child("Chat").child(userID).child("Admin").child(key1).setValue(messageMap).
-                                        addOnCompleteListener(new OnCompleteListener<Void>() {
-                                            @Override
-                                            public void onComplete(@NonNull Task<Void> task) {
-                                                if (task.isSuccessful()) {
-                                                    Toast.makeText(ChatActivity.this, "Mesaj başarı ile Kaydedildi1", Toast.LENGTH_SHORT).show();
-                                                }
-                                            }
-                                        });
-                            }
-                        }
-                    }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    Toast.makeText(ChatActivity.this, "Mesaj gönderme Başarısız1" + e.getMessage(), Toast.LENGTH_LONG).show();
-                }
-            });
-        } else {
-            databaseReference.child("Chat").child(userID).child("Admin").child(key2).setValue(messageMap).
-                    addOnCompleteListener(new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            if (task.isSuccessful()) {
-                                databaseReference.child("Chat").child("Admin").child(userID).child(key1).setValue(messageMap).
-                                        addOnCompleteListener(new OnCompleteListener<Void>() {
-                                            @Override
-                                            public void onComplete(@NonNull Task<Void> task) {
-                                                if (task.isSuccessful()) {
-                                                    Toast.makeText(ChatActivity.this, "Mesaj başarı ile Kaydedildi2", Toast.LENGTH_SHORT).show();
-                                                }
-                                            }
-                                        });
-                            }
-                        }
-                    }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    Toast.makeText(ChatActivity.this, "Mesaj gönderme Başarısız2" + e.getMessage(), Toast.LENGTH_LONG).show();
-                }
-            });
-        }
-
-
-    }
-
 
     public void backButton() {
         imgBackButton.setOnClickListener(new View.OnClickListener() {
@@ -203,12 +145,14 @@ public class ChatActivity extends AppCompatActivity {
         edMesaj = findViewById(R.id.edChatAcitivityMesaj);
         if (firebaseUser.getEmail().equals("diyetisyenadmin@gmail.com")) {
             userName1 = getIntent().getExtras().getString("userName1");
+            otherName = getIntent().getExtras().getString("otherName");
+            System.out.println("gazigeliyor->" + userName1 + "->" + otherName);
         }
 
         // System.out.println("gazigeliyor->"+userName1+"->"+otherName);
     }
 
-    public void bilgileriCek() {
+    public void bilgileriCek2() {
         final String userEmail = firebaseUser.getEmail();
         final String userID = firebaseUser.getUid();
         final String diyetiysyenAdmin = "diyetisyenadmin@gmail.com";
@@ -252,6 +196,51 @@ public class ChatActivity extends AppCompatActivity {
                     });
         }
 
+    }
+
+    public void bilgileriCek() {
+        String adminMail = "diyetisyenadmin@gmail.com";
+        String adminMailFireBase = firebaseUser.getEmail();
+        if (adminMail.equals(adminMailFireBase)) {
+            System.out.println("gazi->pikacu"+otherName);
+
+            System.out.println("gazinull->" + userName1 + "-" + userName + "" + otherName);
+            databaseReference.child("Chat").child(userName1).child("oGeB49XXUsRAlCypndQoUBygPNI3").
+                    addChildEventListener(new ChildEventListener() {
+                        @Override
+                        public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                           // System.out.println("kelle->" + userKellesi + "kelle name " + kelleName + "--");
+                            kullaniciCek();
+                            HashMap<String, String> hashMap = (HashMap<String, String>) dataSnapshot.getValue();
+                            String user = hashMap.get("from");
+                            String text = hashMap.get("text");
+                            ChatModel chatModel = new ChatModel(user, text);
+                            list.add(chatModel);
+                            chatAdapter.notifyDataSetChanged();
+                            chatRecyclerView.scrollToPosition(list.size() - 1);
+                        }
+
+                        @Override
+                        public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                        }
+
+                        @Override
+                        public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+                        }
+
+                        @Override
+                        public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+        }
     }
 
     public void bilgileriCek1() {
@@ -326,5 +315,93 @@ public class ChatActivity extends AppCompatActivity {
                     });
         }*/
 
+    }
+
+    public void kullaniciCek() {
+        databaseReference.child("Kullanicilar").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot1, @Nullable String s) {
+                HashMap<String, String> kelleMap = (HashMap<String, String>) dataSnapshot1.getValue();
+                kelleName = kelleMap.get("user");
+
+                userKellesi = dataSnapshot1.getKey();
+                System.out.println("kelle->" + userKellesi + "kelle name " + kelleName + " other name " + otherName);
+                System.out.println("gazi->userKellesi "+userKellesi);
+                System.out.println("gazi->kelleName"+kelleName);
+                System.out.println("gazi->otherName"+otherName);
+
+
+                System.out.println("gazi->gercekKelle"+gercekKelle);
+                String adminMail = "diyetisyenadmin@gmail.com";
+                String adminMailFireBase = firebaseUser.getEmail();
+                if (kelleName.equals(otherName)) {
+                    gercekKelle = userKellesi;
+
+                if (adminMail.equals(adminMailFireBase)) {
+                    System.out.println("gazi->pikacu"+otherName);
+
+                    System.out.println("gazinull->" + userName1 + "-" + userName + "" + otherName);
+                    databaseReference.child("Chat").child(userName1).child(gercekKelle).
+                            addChildEventListener(new ChildEventListener() {
+                                @Override
+                                public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                                    // System.out.println("kelle->" + userKellesi + "kelle name " + kelleName + "--");
+                                    HashMap<String, String> hashMap = (HashMap<String, String>) dataSnapshot.getValue();
+                                    System.out.println("gazi->userKellesi1"+userKellesi);
+                                    System.out.println("gazi->kelleName1"+kelleName);
+                                    System.out.println("gazi->otherName1"+otherName);
+                                    System.out.println("gazi->gercekKelle1"+gercekKelle);
+                                    String user = hashMap.get("from");
+                                    String text = hashMap.get("text");
+                                    ChatModel chatModel = new ChatModel(user, text);
+                                    list.add(chatModel);
+                                    chatAdapter.notifyDataSetChanged();
+                                    chatRecyclerView.scrollToPosition(list.size() - 1);
+                                }
+
+                                @Override
+                                public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                                }
+
+                                @Override
+                                public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+                                }
+
+                                @Override
+                                public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                }
+                            });
+                }
+                }
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
